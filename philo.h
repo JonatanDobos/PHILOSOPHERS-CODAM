c72104ac0c0745e069a9c3e2f389155db35ba165
Jonatan Dobos <jdobos@f0r3s3.codam.nl>
2024-08-20 20:33:56 +0200

    first

---
+++ philo.h
@@ -0,0 +1,17 @@
+#ifndef PHILO_H
+# define PHILO_H
+# include <stdio.h>
+# include <unistd.h>
+# include <string.h>
+# include <stdlib.h>
+# include <stdarg.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <limits.h>
+# include <sys/wait.h>
+# include <errno.h>
+# include <fcntl.h>
+# include <stdbool.h>
+# include <termios.h>
+
+#endif
