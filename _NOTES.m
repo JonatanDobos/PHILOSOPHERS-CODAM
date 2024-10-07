3f9ae46d64db3356689a5540a80088404d47c76a
Jonatan Dobos <jdobos@f1r2s7.codam.nl>
2024-10-07 17:19:44 +0200

    notes

--- _NOTES.m
+++ _NOTES.m
@@ -1,4 +1,11 @@
 TODO:
+
+- NOTES TO WRITE thread (to battle write() delay)
+- MAX 200 philos so on STACK
+- (maybe static mutex)
+- Change monitor to only checking the last given time of death! (instead of int array)
+- TEST deadlocks!
+
+
 - protect (eg.: gettimeofday())
-- write monitor function
 - TEST!
